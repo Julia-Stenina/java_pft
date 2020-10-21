a63e478291dcca57f7b761230f3d9dce55023e21
Julia <70764952+Julia-Stenina@users.noreply.github.com>
2020-10-21 17:29:51 +0300

    Добавлено наследование классов

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String lastName;
+  private final String nickname;
+  private final String phoneNumber;
+  private final String email;
+
+  public ContactData(String firstName, String lastName, String nickname, String phoneNumber, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.nickname = nickname;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
